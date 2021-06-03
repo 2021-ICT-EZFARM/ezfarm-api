e851658e85365f4273e201536ec572b46e69da0c
highright96 <nsw5296@naver.com>
2021-06-03 23:00:20 +0900

    init/프로젝트 생성

---
+++ src/main/java/com/ezfarm/ezfarmback/EzfarmBackApplication.java
@@ -0,0 +1,13 @@
+package com.ezfarm.ezfarmback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzfarmBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzfarmBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ezfarm/ezfarmback/EzfarmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezfarm.ezfarmback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzfarmBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
