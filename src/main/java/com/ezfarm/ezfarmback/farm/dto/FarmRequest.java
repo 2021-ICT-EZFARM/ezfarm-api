0c7a0eecb6af348cd327219b6a35af67ea0cda2b
highright96 <nsw5296@naver.com>
2021-06-19 21:26:15 +0900

    refactor : 엔티티, dto 일부 변경

--- src/main/java/com/ezfarm/ezfarmback/farm/dto/FarmRequest.java
+++ src/main/java/com/ezfarm/ezfarmback/farm/dto/FarmRequest.java
@@ -2,29 +2,34 @@ package com.ezfarm.ezfarmback.farm.dto;
 
 import com.ezfarm.ezfarmback.farm.domain.enums.CropType;
 import com.ezfarm.ezfarmback.farm.domain.enums.FarmType;
-import com.ezfarm.ezfarmback.user.domain.User;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.Valid;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDateTime;
-
 @Data
-public class FarmForm {
+@NoArgsConstructor
+@AllArgsConstructor
+public class FarmRequest {
 
+    @NotBlank
     private String address;
 
+    @NotBlank
     private String phoneNumber;
 
+    @NotBlank
     private String area;
 
-    private Boolean isMain;
+    private boolean isMain;
 
     private FarmType farmType;
 
     private CropType cropType;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDateTime startDate;
+    private LocalDate startDate;
 
-    private User user;
 }
