c7363202db55f15944a6b0bb3c945a2bd28b434e
highright96 <nsw5296@naver.com>
2021-06-19 21:27:28 +0900

    refactor : 농장 에러 코드 추가

--- src/main/java/com/ezfarm/ezfarmback/common/exception/dto/ErrorCode.java
+++ src/main/java/com/ezfarm/ezfarmback/common/exception/dto/ErrorCode.java
@@ -13,11 +13,10 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(400, "C_001", "적절하지 않은 요청 값입니다."),
     INVALID_TYPE_VALUE(400, "C_002", "요청 값의 타입이 잘못되었습니다."),
     METHOD_NOT_ALLOWED(405, "C_003", "적절하지 않은 HTTP 메소드입니다."),
-
-
     DUPLICATED_EMAIL(400, "C_004", "이미 존재하는 이메일입니다."),
     BAD_LOGIN(400, "C_005", "잘못된 아이디 또는 패스워드입니다."),
-    NON_EXISTENT_USER(404, "C_006", "존재하지 않는 사용자 입니다.");
+    NON_EXISTENT_USER(404, "C_006", "존재하지 않는 사용자 입니다."),
+    INVALID_FARM_START_DATE(400, "C_007", "잘못된 농장 생성일을 입력했습니다.");
 
     private final int status;
     private final String code;
