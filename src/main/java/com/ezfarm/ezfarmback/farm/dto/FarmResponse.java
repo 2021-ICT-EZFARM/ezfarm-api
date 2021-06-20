6a9fc05f1921887c882e0f6205fa677cbc405c23
kang <minseokkang95@gmail.com>
2021-06-21 01:26:14 +0900

    feat : 클라이언트 응답용 dto 생성

---
+++ src/main/java/com/ezfarm/ezfarmback/farm/dto/FarmResponse.java
@@ -0,0 +1,35 @@
+package com.ezfarm.ezfarmback.farm.dto;
+
+import com.ezfarm.ezfarmback.farm.domain.enums.CropType;
+import com.ezfarm.ezfarmback.farm.domain.enums.FarmType;
+import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FarmResponse {
+
+  @NotBlank
+  private String address;
+
+  @NotBlank
+  private String phoneNumber;
+
+  @NotBlank
+  private String area;
+
+  private boolean isMain;
+
+  private FarmType farmType;
+
+  private CropType cropType;
+
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private LocalDate startDate;
+
+}
