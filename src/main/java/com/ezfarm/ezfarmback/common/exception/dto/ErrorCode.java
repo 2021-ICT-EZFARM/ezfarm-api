466a20f0e00ffa74222d7c7e18c23bb6d124f4cd
highright96 <nsw5296@naver.com>
2021-06-29 22:01:04 +0900

    refactor : 에러 코드 세분화

--- src/main/java/com/ezfarm/ezfarmback/common/exception/dto/ErrorCode.java
+++ src/main/java/com/ezfarm/ezfarmback/common/exception/dto/ErrorCode.java
@@ -13,13 +13,17 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(400, "C_001", "적절하지 않은 요청 값입니다."),
     INVALID_TYPE_VALUE(400, "C_002", "요청 값의 타입이 잘못되었습니다."),
     METHOD_NOT_ALLOWED(405, "C_003", "적절하지 않은 HTTP 메소드입니다."),
-    DUPLICATED_EMAIL(400, "C_004", "이미 존재하는 이메일입니다."),
-    BAD_LOGIN(400, "C_005", "잘못된 아이디 또는 패스워드입니다."),
-    NON_EXISTENT_USER(404, "C_006", "존재하지 않는 사용자 입니다."),
-    INVALID_FARM_START_DATE(400, "C_007", "잘못된 농장 생성일을 입력했습니다."),
-    INVALID_FARM_ID(404, "C_008", "잘못된 농장 ID 입니다."),
 
-    ACCESS_DENIED(403, "C_009", "콘텐츠에 접근 권한이 없습니다.");
+    NON_EXISTENT_USER(404, "US_001", "존재하지 않는 사용자입니다."),
+
+    INVALID_FARM_ID(404, "FA_001", "존재하지 않는 농장입니다."),
+    INVALID_FARM_START_DATE(400, "FA_002", "잘못된 농장 생성일을 입력했습니다."),
+
+    FAVORITE_DUPLICATED(400, "FA_001", "이미 즐겨찾기에 등록된 농장입니다."),
+
+    ACCESS_DENIED(403, "AU_001", "콘텐츠에 접근 권한이 없습니다."),
+    BAD_LOGIN(400, "AU_002", "잘못된 아이디 또는 패스워드입니다."),
+    DUPLICATED_EMAIL(400, "AU_003", "이미 존재하는 이메일입니다.");
 
     private final int status;
     private final String code;
