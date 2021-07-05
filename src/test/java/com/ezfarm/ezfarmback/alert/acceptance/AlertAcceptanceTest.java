2262fd9966bce710c393c568456efa3b37040abf
highright96 <nsw5296@naver.com>
2021-07-05 19:31:14 +0900

    test : 알림 범위 조회/수정 단위/통합 테스트 추가

---
+++ src/test/java/com/ezfarm/ezfarmback/alert/acceptance/AlertAcceptanceTest.java
@@ -0,0 +1,94 @@
+package com.ezfarm.ezfarmback.alert.acceptance;
+
+import com.ezfarm.ezfarmback.alert.acceptance.step.AlertAcceptanceStep;
+import com.ezfarm.ezfarmback.alert.dto.AlertRangeRequest;
+import com.ezfarm.ezfarmback.alert.dto.AlertRangeResponse;
+import com.ezfarm.ezfarmback.common.acceptance.AcceptanceStep;
+import com.ezfarm.ezfarmback.common.acceptance.CommonAcceptanceTest;
+import com.ezfarm.ezfarmback.farm.acceptance.step.FarmAcceptanceStep;
+import com.ezfarm.ezfarmback.farm.domain.enums.CropType;
+import com.ezfarm.ezfarmback.farm.domain.enums.FarmType;
+import com.ezfarm.ezfarmback.farm.dto.FarmRequest;
+import com.ezfarm.ezfarmback.user.dto.AuthResponse;
+import com.ezfarm.ezfarmback.user.dto.LoginRequest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("알림 통합 테스트")
+public class AlertAcceptanceTest extends CommonAcceptanceTest {
+
+    AuthResponse authResponse;
+
+    FarmRequest farmRequest;
+
+    AlertRangeRequest alertRangeRequest;
+
+    @BeforeEach
+    @Override
+    public void setUp() {
+        super.setUp();
+        LoginRequest loginRequest = new LoginRequest("test1@email.com", "비밀번호");
+        authResponse = getAuthResponse(loginRequest);
+
+        farmRequest = new FarmRequest(
+            "경기",
+            "테스트 이름",
+            "010-2222-2222",
+            "100",
+            true,
+            FarmType.GLASS,
+            CropType.PAPRIKA,
+            LocalDate.now()
+        );
+
+        alertRangeRequest = new AlertRangeRequest();
+    }
+
+    @DisplayName("알림 범위를 생성한 후 조회한다.")
+    @Test
+    void findAlertRange_after_creation() throws Exception {
+        ExtractableResponse<Response> farmResponse = FarmAcceptanceStep
+            .requestToCreateFarm(authResponse, farmRequest, objectMapper);
+        Long farmId = FarmAcceptanceStep.getLocation(farmResponse);
+
+        ExtractableResponse<Response> response = AlertAcceptanceStep
+            .requestToFindAlertRange(authResponse, farmId);
+        AlertRangeResponse alertRangeResponse = response.jsonPath()
+            .getObject(".", AlertRangeResponse.class);
+
+        AcceptanceStep.assertThatStatusIsOk(response);
+        AlertAcceptanceStep.assertThatFindAlertRange(alertRangeResponse, alertRangeRequest);
+    }
+
+    @DisplayName("알림 범위를 수정한다.")
+    @Test
+    void updateAlertRange() throws Exception {
+        //given
+        alertRangeRequest.setTmpMin(10.1f);
+        alertRangeRequest.setHumidityMax(20.6f);
+
+        //when
+        ExtractableResponse<Response> farmResponse = FarmAcceptanceStep
+            .requestToCreateFarm(authResponse, farmRequest, objectMapper);
+        Long farmId = FarmAcceptanceStep.getLocation(farmResponse);
+
+        Long alertRangeId = AlertAcceptanceStep
+            .requestToFindAlertRange(authResponse, farmId).jsonPath()
+            .getObject(".", AlertRangeResponse.class)
+            .getId();
+        ExtractableResponse<Response> response = AlertAcceptanceStep
+            .requestToUpdateAlertRange(authResponse, alertRangeRequest, alertRangeId, objectMapper);
+
+        AlertRangeResponse alertRangeResponse = AlertAcceptanceStep
+            .requestToFindAlertRange(authResponse, farmId).jsonPath()
+            .getObject(".", AlertRangeResponse.class);
+
+        //then
+        AcceptanceStep.assertThatStatusIsOk(response);
+        AlertAcceptanceStep.assertThatFindAlertRange(alertRangeResponse, alertRangeRequest);
+    }
+}
--- src/test/java/com/ezfarm/ezfarmback/alert/service/AlertServiceTest.java
+++ src/test/java/com/ezfarm/ezfarmback/alert/service/AlertServiceTest.java
@@ -2,6 +2,7 @@ package com.ezfarm.ezfarmback.alert.service;
 
 import static java.util.Optional.ofNullable;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -9,8 +10,11 @@ import com.ezfarm.ezfarmback.alert.domain.AlertRange;
 import com.ezfarm.ezfarmback.alert.domain.AlertRangeRepository;
 import com.ezfarm.ezfarmback.alert.dto.AlertRangeRequest;
 import com.ezfarm.ezfarmback.alert.dto.AlertRangeResponse;
+import com.ezfarm.ezfarmback.common.exception.CustomException;
+import com.ezfarm.ezfarmback.common.exception.dto.ErrorCode;
 import com.ezfarm.ezfarmback.farm.domain.Farm;
 import com.ezfarm.ezfarmback.farm.domain.FarmRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -47,8 +51,7 @@ public class AlertServiceTest {
             .name("테스트 농장")
             .build();
 
-        alertRange = new AlertRange();
-        alertRange.setFarm(farm);
+        alertRange = new AlertRange(farm);
     }
 
     @DisplayName("알림 범위를 조회한다.")
@@ -69,6 +72,32 @@ public class AlertServiceTest {
         );
     }
 
+    @DisplayName("알림 범위가 존재하지 않으면 새로 생성한다.")
+    @Test
+    void findAlertRange_notFound_alertRange_failure() {
+        AlertRangeResponse alertRangeResponse = new AlertRangeResponse();
+        when(farmRepository.findById(any())).thenReturn(ofNullable(farm));
+        when(alertRangeRepository.findByFarm(any())).thenReturn(Optional.empty());
+        when(modelMapper.map(any(), any())).thenReturn(alertRangeResponse);
+
+        AlertRangeResponse result = alertService.findAlertRange(1L);
+
+        Assertions.assertAll(
+            () -> assertThat(result.getTmpMax()).isEqualTo(alertRange.getTmpMax()),
+            () -> assertThat(result.getTmpMin()).isEqualTo(alertRange.getTmpMin()),
+            () -> assertThat(result.getCo2Max()).isEqualTo(alertRange.getCo2Max())
+        );
+    }
+
+    @DisplayName("알림 범위 조회 시 농장이 존재하지 않으면 에러가 발생한다.")
+    @Test
+    void findAlertRange_invalid_farm_failure() {
+        when(farmRepository.findById(any())).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> alertService.findAlertRange(1L))
+            .isInstanceOf(CustomException.class)
+            .hasMessage(ErrorCode.INVALID_FARM_ID.getMessage());
+    }
+
     @DisplayName("알림 범위를 수정한다.")
     @Test
     void updateAlertRange_success() {
@@ -84,5 +113,15 @@ public class AlertServiceTest {
             () -> assertThat(alertRange.getTmpMin()).isEqualTo(alertRangeRequest.getTmpMin()),
             () -> assertThat(alertRange.getCo2Max()).isEqualTo(alertRangeRequest.getCo2Max())
         );
+    }
+
+    @DisplayName("알림 범위 수정 시 엔티티가 없으면 에러를 발생한다.")
+    @Test
+    void updateAlertRange_notFound_alertRange_failure() {
+        AlertRangeRequest alertRangeRequest = new AlertRangeRequest();
+        when(alertRangeRepository.findById(any())).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> alertService.updateAlertRange(1L, alertRangeRequest))
+            .isInstanceOf(CustomException.class)
+            .hasMessage(ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
     }
 }
