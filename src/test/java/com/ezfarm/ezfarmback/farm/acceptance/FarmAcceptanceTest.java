aff3f9e64be2a9016deacf62675751a8948c409a
highright96 <nsw5296@naver.com>
2021-07-08 15:39:30 +0900

    refactor : 농가 통합/단위 테스트 리팩토링

--- src/test/java/com/ezfarm/ezfarmback/farm/acceptance/FarmAcceptanceTest.java
+++ src/test/java/com/ezfarm/ezfarmback/farm/acceptance/FarmAcceptanceTest.java
@@ -2,18 +2,18 @@ package com.ezfarm.ezfarmback.farm.acceptance;
 
 
 import static com.ezfarm.ezfarmback.common.acceptance.AcceptanceStep.assertThatStatusIsOk;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.ezfarm.ezfarmback.common.acceptance.AcceptanceStep;
 import com.ezfarm.ezfarmback.common.acceptance.CommonAcceptanceTest;
 import com.ezfarm.ezfarmback.farm.acceptance.step.FarmAcceptanceStep;
-import com.ezfarm.ezfarmback.farm.domain.enums.CropType;
-import com.ezfarm.ezfarmback.farm.domain.enums.FarmType;
 import com.ezfarm.ezfarmback.farm.dto.FarmRequest;
+import com.ezfarm.ezfarmback.farm.dto.FarmResponse;
 import com.ezfarm.ezfarmback.user.dto.AuthResponse;
 import com.ezfarm.ezfarmback.user.dto.LoginRequest;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,7 +21,9 @@ import org.junit.jupiter.api.Test;
 @DisplayName("농가 통합 테스트")
 public class FarmAcceptanceTest extends CommonAcceptanceTest {
 
-    FarmRequest farmRequest;
+    FarmRequest farmRequest1;
+
+    FarmRequest farmRequest2;
 
     AuthResponse authResponse;
 
@@ -32,94 +34,100 @@ public class FarmAcceptanceTest extends CommonAcceptanceTest {
         LoginRequest loginRequest = new LoginRequest("test1@email.com", "비밀번호");
         authResponse = getAuthResponse(loginRequest);
 
-        farmRequest = new FarmRequest(
-            "경기",
-            "테스트 이름",
-            "010-2222-2222",
-            "100",
-            true,
-            FarmType.GLASS,
-            CropType.PAPRIKA,
-            LocalDate.now()
-        );
+        farmRequest1 = FarmRequest.builder()
+            .name("테스트 농장 이름1")
+            .address("서울")
+            .phoneNumber("010-1234-1234")
+            .isMain(false)
+            .build();
+
+        farmRequest2 = FarmRequest.builder()
+            .name("테스트 농장 이름2")
+            .address("경기")
+            .phoneNumber("010-1234-1234")
+            .isMain(false)
+            .build();
     }
 
-    @DisplayName("농가을 생성한다.")
+    @DisplayName("나의 농가를 생성한다.")
     @Test
     void createFarm() throws Exception {
-        //when
-        ExtractableResponse<Response> response = FarmAcceptanceStep
-            .requestToCreateFarm(authResponse, farmRequest, objectMapper);
+        ExtractableResponse<Response> createResponse = FarmAcceptanceStep
+            .requestToCreateFarm(authResponse, farmRequest1, objectMapper);
+        Long farmId = FarmAcceptanceStep.getLocation(createResponse);
 
-        //then
-        AcceptanceStep.assertThatStatusIsCreated(response);
+        ExtractableResponse<Response> findResponse = FarmAcceptanceStep
+            .requestToFindMyFarm(authResponse, farmId);
+        FarmResponse farmResponse = findResponse.jsonPath().getObject(".", FarmResponse.class);
+
+        AcceptanceStep.assertThatStatusIsCreated(createResponse);
+        FarmAcceptanceStep.assertThatFindMyFarm(farmResponse, farmRequest1);
     }
 
-    @DisplayName("농가를 수정한다.")
+    @DisplayName("나의 농가를 수정한다.")
     @Test
     void updateFarm() throws Exception {
-        FarmRequest updateRequest = new FarmRequest(
-            "서울",
-            "테스트 이름",
-            "010-3333-3333",
-            "200",
-            false,
-            FarmType.VINYL,
-            CropType.PAPRIKA,
-            LocalDate.now().plusDays(1)
-        );
+        FarmRequest updateRequest = FarmRequest.builder()
+            .name("테스트 농장 이름3")
+            .address("경기")
+            .isMain(true)
+            .build();
 
-        String url = FarmAcceptanceStep
-            .requestToCreateFarmAndGetLocation(authResponse, farmRequest, objectMapper);
-        ExtractableResponse<Response> response = FarmAcceptanceStep
-            .requestToUpdateFarm(url, authResponse, updateRequest, objectMapper);
+        Long farmId = FarmAcceptanceStep
+            .requestToCreateFarmAndGetLocation(authResponse, farmRequest1, objectMapper);
+        ExtractableResponse<Response> updateResponse = FarmAcceptanceStep
+            .requestToUpdateFarm(authResponse, updateRequest, objectMapper, farmId);
 
-        assertThatStatusIsOk(response);
+        ExtractableResponse<Response> findResponse = FarmAcceptanceStep
+            .requestToFindMyFarm(authResponse, farmId);
+        FarmResponse farmResponse = findResponse.jsonPath().getObject(".", FarmResponse.class);
+
+        assertThatStatusIsOk(updateResponse);
+        FarmAcceptanceStep.assertThatFindMyFarm(farmResponse, updateRequest);
     }
 
-    @DisplayName("모든 농가를 조회한다.")
+    @DisplayName("나의 모든 농가를 조회한다.")
     @Test
-    void findAllFarm() throws Exception {
-        FarmRequest updateRequest = new FarmRequest(
-            "서울",
-            "테스트 이름",
-            "010-3333-3333",
-            "200",
-            false,
-            FarmType.VINYL,
-            CropType.PAPRIKA,
-            LocalDate.now().plusDays(1)
-        );
+    void findMyFarms() throws Exception {
 
-        FarmAcceptanceStep.requestToCreateFarm(authResponse, farmRequest, objectMapper);
-        FarmAcceptanceStep.requestToCreateFarm(authResponse, updateRequest, objectMapper);
+        FarmAcceptanceStep.requestToCreateFarm(authResponse, farmRequest1, objectMapper);
+        FarmAcceptanceStep.requestToCreateFarm(authResponse, farmRequest2, objectMapper);
+
         ExtractableResponse<Response> response = FarmAcceptanceStep
-            .requestToFindFarms(authResponse);
+            .requestToFindMyFarms(authResponse);
+        List<FarmResponse> farmResponses = response.jsonPath().getList(".", FarmResponse.class);
 
         assertThatStatusIsOk(response);
-        FarmAcceptanceStep.assertThatFindTwoFarms(response);
+        FarmAcceptanceStep.assertThatFindMyFarms(farmResponses, farmRequest1, farmRequest2);
     }
 
-    @DisplayName("농가를 조회한다.")
+    @DisplayName("나의 농가를 조회한다.")
     @Test
-    void viweFarm() throws Exception {
-        String url = FarmAcceptanceStep
-            .requestToCreateFarmAndGetLocation(authResponse, farmRequest, objectMapper);
+    void findMyFarm() throws Exception {
+        Long farmId = FarmAcceptanceStep
+            .requestToCreateFarmAndGetLocation(authResponse, farmRequest1, objectMapper);
         ExtractableResponse<Response> response = FarmAcceptanceStep
-            .requestToFindFarm(url, authResponse);
+            .requestToFindMyFarm(authResponse, farmId);
+        FarmResponse farmResponse = response.jsonPath().getObject(".", FarmResponse.class);
 
-        FarmAcceptanceStep.assertThatFindFarm(response);
+        AcceptanceStep.assertThatStatusIsOk(response);
+        FarmAcceptanceStep.assertThatFindMyFarm(farmResponse, farmRequest1);
     }
 
     @DisplayName("농가를 삭제한다.")
     @Test
     void deleteFarm() throws Exception {
-        String url = FarmAcceptanceStep
-            .requestToCreateFarmAndGetLocation(authResponse, farmRequest, objectMapper);
-        ExtractableResponse<Response> response = FarmAcceptanceStep.requestToDeleteFarm(url, authResponse);
+        Long farmId = FarmAcceptanceStep
+            .requestToCreateFarmAndGetLocation(authResponse, farmRequest1, objectMapper);
 
-        FarmAcceptanceStep.assertThatStatusIsNoContent(response);
+        ExtractableResponse<Response> deleteResponse = FarmAcceptanceStep
+            .requestToDeleteMyFarm(authResponse, farmId);
+
+        List<FarmResponse> farmResponses = FarmAcceptanceStep.requestToFindMyFarms(authResponse)
+            .jsonPath()
+            .getList(".", FarmResponse.class);
+
+        AcceptanceStep.assertThatStatusIsNoContent(deleteResponse);
+        assertThat(farmResponses.size()).isEqualTo(0);
     }
-
-
 }
--- src/test/java/com/ezfarm/ezfarmback/farm/service/FarmServiceTest.java
+++ src/test/java/com/ezfarm/ezfarmback/farm/service/FarmServiceTest.java
@@ -1,5 +1,6 @@
 package com.ezfarm.ezfarmback.farm.service;
 
+import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,8 +14,6 @@ import com.ezfarm.ezfarmback.common.exception.CustomException;
 import com.ezfarm.ezfarmback.common.exception.dto.ErrorCode;
 import com.ezfarm.ezfarmback.farm.domain.Farm;
 import com.ezfarm.ezfarmback.farm.domain.FarmRepository;
-import com.ezfarm.ezfarmback.farm.domain.enums.CropType;
-import com.ezfarm.ezfarmback.farm.domain.enums.FarmType;
 import com.ezfarm.ezfarmback.farm.dto.FarmRequest;
 import com.ezfarm.ezfarmback.farm.dto.FarmResponse;
 import com.ezfarm.ezfarmback.user.domain.Role;
@@ -23,7 +22,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -59,58 +57,39 @@ public class FarmServiceTest {
 
         user = User.builder()
             .id(1L)
-            .name("남상우")
-            .email("a@gmail.com")
+            .name("테스트 이름")
+            .email("test@email.com")
             .password("비밀번호")
             .role(Role.ROLE_USER)
             .build();
 
-        farmRequest = new FarmRequest(
-            "경기",
-            "테스트 이름",
-            "010-2222-2222",
-            "100",
-            true,
-            FarmType.GLASS,
-            CropType.PAPRIKA,
-            null
-        );
-
-        farmResponse = new FarmResponse(
-            1L,
-            "경기",
-            "농가 이름",
-            "010-2222-2222",
-            "100",
-            true,
-            FarmType.GLASS,
-            CropType.PAPRIKA,
-            LocalDate.now()
-        );
-
         farm = Farm.builder()
-            .address("경기")
-            .phoneNumber("010-2222-2222")
-            .area("100")
-            .farmType(FarmType.GLASS)
-            .cropType(CropType.PAPRIKA)
+            .name("테스트 농장 이름1")
+            .address("서울")
             .isMain(false)
             .startDate(LocalDate.now())
             .build();
-        farm.setCreatedDate(LocalDateTime.now());
+
+        farmRequest = FarmRequest.builder()
+            .name("테스트 농장 이름2")
+            .address("경기")
+            .build();
+
+        farmResponse = FarmResponse.builder()
+            .id(1L)
+            .name("테스트 농장 이름1")
+            .address("서울")
+            .build();
     }
 
     @DisplayName("농가 시작일이 없는 농가를 생성한다.")
     @Test
     void createFarm_startDate_null_success() {
-        //given
         when(modelMapper.map(any(), any())).thenReturn(farm);
         when(farmRepository.save(any())).thenReturn(farm);
 
-        //when
         farmService.createFarm(user, farmRequest);
 
-        //then
         assertThat(farm.getUser()).isEqualTo(user);
         verify(farmRepository).save(any());
     }
@@ -118,15 +97,12 @@ public class FarmServiceTest {
     @DisplayName("농가 시작일이 농가 생성일 이후인 농가를 생성한다.")
     @Test
     void createFarm_startDate_notNull_success() {
-        //given
         when(modelMapper.map(any(), any())).thenReturn(farm);
         when(farmRepository.save(any())).thenReturn(farm);
         farmRequest.setStartDate(LocalDate.now());
 
-        //when
         farmService.createFarm(user, farmRequest);
 
-        //then
         assertThat(farm.getUser()).isEqualTo(user);
         verify(modelMapper).map(any(), any());
     }
@@ -134,10 +110,8 @@ public class FarmServiceTest {
     @DisplayName("농가 시작일이 농가 생성일보다 과거이면 예외가 발생한다.")
     @Test
     void createFarm_failure_customException() {
-        //given
         farmRequest.setStartDate(LocalDate.of(2000, 1, 1));
 
-        //when, then
         assertThatThrownBy(() -> farmService.createFarm(user, farmRequest))
             .isInstanceOf(CustomException.class)
             .hasMessage(ErrorCode.INVALID_FARM_START_DATE.getMessage());
@@ -146,7 +120,7 @@ public class FarmServiceTest {
     @DisplayName("나의 모든 농가를 조회한다.")
     @Test
     void findMyFarms_success() {
-        farm.addOwner(user);
+        farm.setUser(user);
         when(farmRepository.findAllByUser(any())).thenReturn(Collections.singletonList(farm));
         when(modelMapper.map(any(), any())).thenReturn(farmResponse);
 
@@ -172,7 +146,7 @@ public class FarmServiceTest {
     @DisplayName("존재하지 않는 농가일 경우 예외가 발생한다")
     @Test
     void findFarm_is_not_owner_failure() {
-        when(farmRepository.findById(any())).thenReturn(Optional.empty());
+        when(farmRepository.findById(any())).thenReturn(empty());
 
         assertThatThrownBy(() -> farmService.findMyFarm(1L))
             .isInstanceOf(CustomException.class)
@@ -182,85 +156,48 @@ public class FarmServiceTest {
     @DisplayName("나의 농가를 수정한다.")
     @Test
     void updateFarm_success() {
-        FarmRequest farmRequest2 = new FarmRequest(
-            "부산",
-            "테스트 이름",
-            "010-3333-3333",
-            "200",
-            true,
-            FarmType.VINYL,
-            CropType.TOMATO,
-            LocalDate.now().plusDays(1)
-        );
+        farm.setUser(user);
         when(farmRepository.findById(any())).thenReturn(ofNullable(farm));
 
-        farmService.updateMyFarm(user, 1L, farmRequest2);
+        farmService.updateMyFarm(user, 1L, farmRequest);
 
         Assertions.assertAll(
-            () -> assertThat(farm.getAddress()).isEqualTo(farmRequest2.getAddress()),
-            () -> assertThat(farm.getPhoneNumber()).isEqualTo(farmRequest2.getPhoneNumber()),
-            () -> assertThat(farm.isMain()).isEqualTo(farmRequest2.isMain()),
-            () -> assertThat(farm.getFarmType()).isEqualTo(farmRequest2.getFarmType()),
-            () -> assertThat(farm.getCropType()).isEqualTo(farmRequest2.getCropType())
+            () -> assertThat(farm.getAddress()).isEqualTo(farmRequest.getAddress()),
+            () -> assertThat(farm.getName()).isEqualTo(farmRequest.getName())
         );
     }
 
-    @DisplayName("새로운 메인 농가으로 수정한다.")
+    @DisplayName("새로운 메인 농가로 수정한다.")
     @Test
     void updateFarm_success_change_main() {
-        Farm farm2 = Farm.builder()
-            .address("경기")
-            .phoneNumber("010-2222-2222")
-            .area("100")
-            .farmType(FarmType.GLASS)
-            .cropType(CropType.PAPRIKA)
+        farm.setUser(user);
+        farmRequest.setMain(true);
+        Farm prevMainFarm = Farm.builder()
             .isMain(true)
-            .startDate(LocalDate.now())
             .build();
-        farm2.setCreatedDate(LocalDateTime.now());
-        farm2.addOwner(user);
-        farm.addOwner(user);
 
-        FarmRequest farmRequest2 = new FarmRequest(
-            "부산",
-            "테스트 이름",
-            "010-3333-3333",
-            "200",
-            true,
-            FarmType.VINYL,
-            CropType.TOMATO,
-            LocalDate.now().plusDays(1)
-        );
         when(farmRepository.findById(any())).thenReturn(ofNullable(farm));
-        when(farmRepository.findByIsMainAndUser(anyBoolean(), any())).thenReturn(of(farm2));
+        when(farmRepository.findByIsMainAndUser(anyBoolean(), any()))
+            .thenReturn(of(prevMainFarm));
 
-        farmService.updateMyFarm(user, 1L, farmRequest2);
+        farmService.updateMyFarm(user, 1L, farmRequest);
 
         Assertions.assertAll(
-            () -> assertThat(farm.isMain()).isEqualTo(farmRequest2.isMain()),
-            () -> assertThat(farm2.isMain()).isEqualTo(false)
+            () -> assertThat(farm.isMain()).isEqualTo(true),
+            () -> assertThat(prevMainFarm.isMain()).isEqualTo(false)
         );
     }
 
     @DisplayName("농가 재배 시작 일자가 농가 생성 일자보다 빠르면 예외가 발생한다.")
     @Test
     void updateFarm_failure_startDateException() {
-        farm.addOwner(user);
-
-        FarmRequest farmRequest2 = new FarmRequest(
-            "부산",
-            "테스트 이름",
-            "010-3333-3333",
-            "200",
-            true,
-            FarmType.VINYL,
-            CropType.TOMATO,
-            LocalDate.now().minusDays(1)
-        );
+        farm.setUser(user);
+        farm.setCreatedDate(LocalDateTime.now());
+        farmRequest.setStartDate(LocalDate.now().minusDays(1));
 
         when(farmRepository.findById(any())).thenReturn(ofNullable(farm));
 
-        assertThatThrownBy(() -> farmService.updateMyFarm(user, 1L, farmRequest2))
+        assertThatThrownBy(() -> farmService.updateMyFarm(user, 1L, farmRequest))
             .isInstanceOf(CustomException.class)
             .hasMessage(ErrorCode.INVALID_FARM_START_DATE.getMessage());
     }
@@ -268,7 +205,7 @@ public class FarmServiceTest {
     @DisplayName("나의 농가를 삭제한다.")
     @Test
     void deleteFarm_success() {
-        farm.addOwner(user);
+        farm.setUser(user);
         when(farmRepository.findById(any())).thenReturn(ofNullable(farm));
         farmService.deleteMyFarm(user, 1L);
 
@@ -278,7 +215,7 @@ public class FarmServiceTest {
     @DisplayName("자신의 농가가 아닌 농가를 삭제하면 예외가 발생한다.")
     @Test
     void deleteFarm_access_denied_failure() {
-        farm.addOwner(User.builder().id(2L).build());
+        farm.setUser(User.builder().id(2L).build());
         when(farmRepository.findById(any())).thenReturn(ofNullable(farm));
 
         assertThatThrownBy(() -> farmService.deleteMyFarm(user, 1L))
