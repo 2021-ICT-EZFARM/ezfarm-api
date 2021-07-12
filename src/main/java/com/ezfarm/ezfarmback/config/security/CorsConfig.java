7357038814585f07e98b75c38304f66ba1b1131f
highright96 <nsw5296@naver.com>
2021-07-12 22:38:43 +0900

    refactor : corsFilter 수정

--- src/main/java/com/ezfarm/ezfarmback/config/security/CorsConfig.java
+++ src/main/java/com/ezfarm/ezfarmback/config/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ezfarm.ezfarmback.config.security;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,12 +9,13 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); //json 응답 가능
-        config.addAllowedOrigin("*"); //모든 ip 허용
+        config.setAllowedOriginPatterns(Collections.singletonList("*")); //모든 ip 허용
         config.addAllowedHeader("*"); //모든 header 혀용
         config.addAllowedMethod("*"); //모든 메서드 허용
         source.registerCorsConfiguration("/**", config);
