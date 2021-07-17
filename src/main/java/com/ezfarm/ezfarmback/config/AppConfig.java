ed9411890c2377d794ea9ebc0d74c2c195a07a38
highright96 <nsw5296@naver.com>
2021-07-18 03:25:14 +0900

    feat : querydsl 추가, JPAQueryFactory 빈 등록

--- src/main/java/com/ezfarm/ezfarmback/config/AppConfig.java
+++ src/main/java/com/ezfarm/ezfarmback/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.ezfarm.ezfarmback.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
@@ -9,31 +12,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper =new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT)
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(AccessLevel.PRIVATE)
-                .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE)
-                .setSourceNameTokenizer(NameTokenizers.UNDERSCORE);
+            .setMatchingStrategy(MatchingStrategies.STRICT)
+            .setFieldMatchingEnabled(true)
+            .setFieldAccessLevel(AccessLevel.PRIVATE)
+            .setDestinationNameTokenizer(NameTokenizers.UNDERSCORE)
+            .setSourceNameTokenizer(NameTokenizers.UNDERSCORE);
         return modelMapper;
     }
 
     @Bean
     public CharacterEncodingFilter characterEncodingFilter() {
-
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-
         characterEncodingFilter.setEncoding("UTF-8");
-
         characterEncodingFilter.setForceEncoding(true);
-
         return characterEncodingFilter;
-
     }
 
 }
