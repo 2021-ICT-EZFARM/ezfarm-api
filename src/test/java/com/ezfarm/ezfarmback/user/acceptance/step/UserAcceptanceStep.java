cd1fdfcd8d9162ee67434cf444c11868c26c8c79
highright96 <nsw5296@naver.com>
2021-07-18 21:58:10 +0900

    test : 파일 업로드 통합/단위 테스트 추가

--- src/test/java/com/ezfarm/ezfarmback/user/acceptance/step/UserAcceptanceStep.java
+++ src/test/java/com/ezfarm/ezfarmback/user/acceptance/step/UserAcceptanceStep.java
@@ -1,15 +1,37 @@
 package com.ezfarm.ezfarmback.user.acceptance.step;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import com.ezfarm.ezfarmback.user.domain.User;
 import com.ezfarm.ezfarmback.user.dto.AuthResponse;
-import com.ezfarm.ezfarmback.user.dto.UserUpdateRequest;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ezfarm.ezfarmback.user.dto.UserResponse;
+import io.restassured.builder.MultiPartSpecBuilder;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 import org.springframework.http.MediaType;
 
 public class UserAcceptanceStep {
+
+    public static void assertThatFindUser(UserResponse userResponse, User user) {
+        assertAll(
+            () -> assertThat(userResponse.getEmail()).isEqualTo(user.getEmail()),
+            () -> assertThat(userResponse.getId()).isNotNull(),
+            () -> assertThat(userResponse.getRole()).isEqualTo(user.getRole()),
+            () -> assertThat(userResponse.getName()).isEqualTo(user.getName())
+        );
+    }
+
+    public static void assertThatUpdateUser(UserResponse userResponse) {
+        assertAll(
+            () -> assertThat(userResponse.getAddress()).isEqualTo("수정"),
+            () -> assertThat(userResponse.getPhoneNumber()).isEqualTo("010-1111-1111"),
+            () -> assertThat(userResponse.getImageUrl()).isNotNull()
+        );
+    }
 
     public static ExtractableResponse<Response> requestToReadUser(
         AuthResponse authResponse) {
@@ -22,14 +44,26 @@ public class UserAcceptanceStep {
             .extract();
     }
 
-    public static ExtractableResponse<Response> requestToUpdateUser(AuthResponse authResponse,
-        UserUpdateRequest userUpdateRequest, ObjectMapper objectMapper)
+    public static ExtractableResponse<Response> requestToUpdateUser(AuthResponse authResponse)
         throws Exception {
         return given().log().all()
             .header("Authorization",
                 authResponse.getTokenType() + " " + authResponse.getAccessToken())
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(objectMapper.writeValueAsString(userUpdateRequest))
+            .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+            .multiPart(new MultiPartSpecBuilder("010-1111-1111")
+                .charset(StandardCharsets.UTF_8)
+                .controlName("phoneNumber")
+                .build())
+            .multiPart(new MultiPartSpecBuilder("수정")
+                .charset(StandardCharsets.UTF_8)
+                .controlName("address")
+                .build())
+            .multiPart(new MultiPartSpecBuilder(
+                new FileInputStream("src/test/resources/images/ezfarm-logo.PNG"))
+                .fileName("images.png")
+                .controlName("image")
+                .mimeType(MediaType.IMAGE_JPEG_VALUE)
+                .build())
             .when()
             .patch("/api/user")
             .then().log().all()
