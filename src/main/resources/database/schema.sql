c691be06519fdb5b4258bca599b78a8e47f5fe5f
highright96 <nsw5296@naver.com>
2021-07-19 21:36:02 +0900

    feat : 시설 테이블 생성 쿼리 추가

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,7 +1,7 @@
 --table--
 create table user
 (
-    user_id      bigint(20) NOT NULL AUTO_INCREMENT,
+    user_id      bigint(20)   NOT NULL AUTO_INCREMENT,
     name         varchar(20)  NOT NULL,
     email        varchar(50)  NOT NULL,
     password     varchar(255) NOT NULL,
@@ -12,24 +12,24 @@ create table user
     created_date timestamp,
     updated_date timestamp,
     primary key (user_id)
-)
+);
 
 create table farm
 (
-    farm_id      bigint(20) NOT NULL AUTO_INCREMENT,
-    user_id      bigint(20) NOT NULL,
+    farm_id      bigint(20)  NOT NULL AUTO_INCREMENT,
+    user_id      bigint(20)  NOT NULL,
     name         varchar(50) NOT NULL,
     is_main      boolean     NOT NULL,
-    phone_number varchar(50)  DEFAULT NULL,
-    address      varchar(100) DEFAULT NULL,
-    area         varchar(50)  DEFAULT NULL,
-    crop_type    enum('TOMATO', 'STRAWBERRY', 'PAPRIKA') DEFAULT NULL,
-    farm_type    enum('VINYL', 'GLASS') DEFAULT NULL,
-    start_date   date         DEFAULT NULL,
+    phone_number varchar(50)                              DEFAULT NULL,
+    address      varchar(100)                             DEFAULT NULL,
+    area         varchar(50)                              DEFAULT NULL,
+    crop_type    enum ('TOMATO', 'STRAWBERRY', 'PAPRIKA') DEFAULT NULL,
+    farm_type    enum ('VINYL', 'GLASS')                  DEFAULT NULL,
+    start_date   date                                     DEFAULT NULL,
     created_date timestamp,
     updated_date timestamp,
     primary key (farm_id)
-)
+);
 
 create table favorite
 (
@@ -37,41 +37,56 @@ create table favorite
     farm_id     bigint(20) NOT NULL,
     user_id     bigint(20) NOT NULL,
     primary key (favorite_id)
-)
+);
 
 create table remote
 (
-    remote_id    bigint(20) NOT NULL AUTO_INCREMENT,
-    farm_id      bigint(20) NOT NULL,
-    co2          enum('ON','OFF') NOT NULL,
-    illuminance  enum('ON','OFF') NOT NULL,
-    temperature  float NOT NULL,
-    water        enum('ON','OFF') NOT NULL,
+    remote_id    bigint(20)        NOT NULL AUTO_INCREMENT,
+    farm_id      bigint(20)        NOT NULL,
+    co2          enum ('ON','OFF') NOT NULL,
+    illuminance  enum ('ON','OFF') NOT NULL,
+    temperature  float             NOT NULL,
+    water        enum ('ON','OFF') NOT NULL,
     created_date timestamp,
     updated_date timestamp,
     primary key (remote_id)
-)
+);
 
 create table alert_range
 (
     alert_range_id bigint(20) NOT NULL AUTO_INCREMENT,
     farm_id        bigint(20) NOT NULL,
-    co2_max        float NOT NULL,
-    co2_min        float NOT NULL,
-    humidity_max   float NOT NULL,
-    humidity_min   float NOT NULL,
-    imn_max        float NOT NULL,
-    imn_min        float NOT NULL,
-    mos_max        float NOT NULL,
-    mos_min        float NOT NULL,
-    ph_max         float NOT NULL,
-    ph_min         float NOT NULL,
-    tmp_max        float NOT NULL,
-    tmp_min        float NOT NULL,
+    co2_max        float      NOT NULL,
+    co2_min        float      NOT NULL,
+    humidity_max   float      NOT NULL,
+    humidity_min   float      NOT NULL,
+    imn_max        float      NOT NULL,
+    imn_min        float      NOT NULL,
+    mos_max        float      NOT NULL,
+    mos_min        float      NOT NULL,
+    ph_max         float      NOT NULL,
+    ph_min         float      NOT NULL,
+    tmp_max        float      NOT NULL,
+    tmp_min        float      NOT NULL,
     created_date   timestamp,
     updated_date   timestamp,
     primary key (alert_range_id)
-)
+);
+
+create table facility
+(
+    facility_id  BIGINT(20) NOT NULL AUTO_INCREMENT,
+    farm_id      BIGINT(20) NOT NULL,
+    tmp          FLOAT DEFAULT NULL,
+    co2          FLOAT DEFAULT NULL,
+    humidity     FLOAT DEFAULT NULL,
+    illuminance  FLOAT DEFAULT NULL,
+    mos          FLOAT DEFAULT NULL,
+    ph           FLOAT DEFAULT NULL,
+    created_date TIMESTAMP  NOT NULL,
+    updated_date TIMESTAMP,
+    primary key (facility_id)
+);
 
 --fk--
 alter table alert_range
@@ -83,7 +98,6 @@ alter table farm
     add constraint farm_user_id_fkey
         foreign key (user_id)
             references user (user_id);
-
 
 alter table favorite
     add constraint favorite_farm_id_fkey
@@ -97,5 +111,10 @@ alter table favorite
 
 alter table remote
     add constraint remote_farm_id_fkey
+        foreign key (farm_id)
+            references farm (farm_id);
+
+alter table facility
+    add constraint facility_farm_id_fkey
         foreign key (farm_id)
             references farm (farm_id);
