8a979a88c542900d290e4d1b5e6e33ba6315587c
highright96 <nsw5296@naver.com>
2021-07-28 14:56:44 +0900

    feat : 실시간 화면 도메인, 생성 쿼리 추가

---
+++ src/main/java/com/ezfarm/ezfarmback/screen/domain/Screen.java
@@ -0,0 +1,34 @@
+package com.ezfarm.ezfarmback.screen.domain;
+
+import com.ezfarm.ezfarmback.farm.domain.Farm;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Screen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "screen_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "farm_id")
+    private Farm farm;
+
+    private String imageUrl;
+
+    private float cropCondition;
+
+    private String measureTime;
+}
