e4cf8d2622911b582dcdb1191306bdfc314c4728
kang <minseokkang95@gmail.com>
2021-08-25 22:07:15 +0900

    feat : 내 농가 센서값의 월 평균 저장

--- src/main/java/com/ezfarm/ezfarmback/facility/domain/month/FacilityMonthAvgRepository.java
+++ src/main/java/com/ezfarm/ezfarmback/facility/domain/month/FacilityMonthAvgRepository.java
@@ -1,10 +1,12 @@
 package com.ezfarm.ezfarmback.facility.domain.month;
 
+import com.ezfarm.ezfarmback.facility.domain.week.FacilityWeekRepositoryCustom;
 import com.ezfarm.ezfarmback.farm.domain.Farm;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FacilityMonthAvgRepository extends JpaRepository<FacilityMonthAvg, Long> {
+public interface FacilityMonthAvgRepository extends JpaRepository<FacilityMonthAvg, Long>,
+    FacilityMonthRepositoryCustom {
 
     List<FacilityMonthAvg> findAllByFarmAndMeasureDateStartsWith(Farm farm, String measureDate);
 
